b8dca31aea10e5aed6b7ad76df6640f9e61ce227
Mahesh Khatri <dev@maheshkhatri.in>
2016-11-02 08:20:44 +0530

    added debug, inquirer and json-stable-stringify npm modules also added gitignore file

--- bin/ilint.js
+++ bin/ilint.js
@@ -1,2 +1,34 @@
 #! /usr/bin/env node
 'use strict';
+
+/**
+ * @fileoverview Main CLI that is run via the ilint command.
+ * @author Mahesh Khatri
+ */
+
+const init = (process.argv.indexOf("--init") > -1),
+  debug = (process.argv.indexOf("--debug") > -1);
+
+// must do this initialization *before* other requires in order to work
+if (debug) {
+  require("debug").enable("ilint:*,-ilint:code-path");
+}
+
+// other required modules
+const cli = require('../lib/api'),
+  fs = require("fs"),
+  path = require("path");
+
+// exception handling
+process.on("uncaughtException", function(err) {
+  console.log(err.message);
+  console.log(err.stack);
+  process.exit(1);
+});
+
+// If its a init command the creat ilintrc file else call the execute method of api
+if (init) {
+
+} else {
+
+}
