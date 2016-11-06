8fe941769cc60b11a2f6fa636265d26d79075025
Mahesh Khatri <dev@maheshkhatri.in>
2016-11-06 08:44:32 +0530

    added .ilintrc file generator and reader

--- bin/ilint.js
+++ bin/ilint.js
@@ -6,21 +6,21 @@
  * @author Mahesh Khatri
  */
 
-const init = (process.argv.indexOf("--init") > -1),
-  debug = (process.argv.indexOf("--debug") > -1);
+const init = (process.argv.indexOf('--init') > -1),
+  debug = (process.argv.indexOf('--debug') > -1);
 
 // must do this initialization *before* other requires in order to work
 if (debug) {
-  require("debug").enable("ilint:*,-ilint:code-path");
+  require('debug').enable('ilint:*,-ilint:code-path');
 }
 
 // other required modules
 const cli = require('../lib/api'),
-  fs = require("fs"),
-  path = require("path");
+  fs = require('fs'),
+  path = require('path');
 
 // exception handling
-process.on("uncaughtException", function(err) {
+process.on('uncaughtException', function(err) {
   console.log(err.message);
   console.log(err.stack);
   process.exit(1);
@@ -28,7 +28,17 @@ process.on("uncaughtException", function(err) {
 
 // If its a init command the creat ilintrc file else call the execute method of api
 if (init) {
+  const configInit = require('../lib/config/config_initializer');
 
+  configInit.initializeConfig(function(err) {
+    if (err) {
+      process.exitCode = 1;
+      console.error(err.message);
+      console.error(err.stack);
+    } else {
+      process.exitCode = 0;
+    }
+  });
 } else {
 
 }
